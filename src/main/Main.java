77d17c8669de1f509755bd729bcb95d404b8187d
Simon <simonsg03@gmail.com>
2025-02-22 23:54:42 +0100

    Modificacion de comentario en Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -29,7 +29,7 @@ public class Main {
         profesor1.calificarAlumno(alumno1, programacion, 90);
         profesor1.calificarAlumno(alumno2, programacion, 78);
 
-        // Mostrar notas de los alumnos
+        // Mostrar notas de los alumnos 1 y 2
         alumno1.mostrarNotas();
         alumno2.mostrarNotas();
     }
